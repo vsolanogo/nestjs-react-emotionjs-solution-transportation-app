18a65c7c23df42762227993e37f7f2871303b630
skreeauk <skreeauk@gmail.com>
2023-06-03 15:38:22 +0300

    docker

--- delivery-react/src/api/api.ts
+++ delivery-react/src/api/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// const apiUrl = "http://localhost:3000"; 
 const apiUrl = "/api"; 
 
 const urls = {
--- delivery-react/vite.config.ts
+++ delivery-react/vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     port: 3001,
+    host: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [
     react({
